0f6003c43048591be5ef3015200fb113a2e5875c
ChrisLoucaides <christosloucaides@gmail.com>
2024-03-30 16:53:11 +0000

    FYP-19 Donut component

---
+++ src/components/StudentDonutChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Doughnut :data="chartData" :options="{maintainAspectRatio: true}" style="width: 100%; height: 350px;"></Doughnut>
+    <h2>My Student Attendance Ratio</h2>
+  </div>
+</template>
+
+<script setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "vue-chartjs";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const chartData = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "Student Attendance",
+      data: [300, 50, 100],
+      backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+</script>
+
+<style scoped>
+
+</style>
