d1b51af698f37931378a12b64bb982248188f4f4
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-13 17:03:17 +0100

    chainging bootstrap example modal ID to createTask ID

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -12,7 +12,7 @@
           <div v-if="tasks.length">
             <h2 class="to-do-list-heading">To Do List</h2>
             <div class="flex-center">
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createTask">
                 Create Task
               </button>
             </div>
@@ -23,7 +23,7 @@
           </div>
           <div v-else>
             <div class="flex-center">
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createTask">
                 Create Task
               </button>
             </div>
@@ -36,11 +36,11 @@
   </main>
 
   <div>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="createTask" tabindex="-1" aria-labelledby="createTaskLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Create Task</h5>
+            <h5 class="modal-title" id="createTaskLabel">Create Task</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
