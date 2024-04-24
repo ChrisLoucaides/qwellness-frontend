ae5389c969e82e3642be06d4c150109e9deb0e83
ChrisLoucaides <christosloucaides@gmail.com>
2024-04-24 23:03:21 +0100

    FYP-38 sorting tasks according to due date

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -9,7 +9,7 @@
           <p>Loading tasks...</p>
         </div>
         <div v-else>
-          <div v-if="tasks.length">
+          <div v-if="sortedTasks.length">
             <h2 class="to-do-list-heading">To Do List</h2>
             <div class="flex-center">
               <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createTask">
@@ -19,7 +19,7 @@
             </div>
             <hr>
             <div class="task-scroll-container" v-motion-slide-visible-bottom>
-              <Task v-for="task in tasks" :key="task.id" :task="task" @edit-task="updateTask" v-motion-slide-top/>
+              <Task v-for="task in sortedTasks" :key="task.id" :task="task" @edit-task="updateTask" v-motion-slide-top/>
             </div>
           </div>
           <div v-else>
@@ -72,19 +72,22 @@
 </template>
 
 <script setup>
-import {ref, onMounted, computed} from 'vue'
-import {useUserStore} from "../../auth.ts";
-import {getCookie} from "../utils/utils.js";
-import Task from "../components/student-components/Task.vue";
+import { ref, onMounted, computed } from 'vue'
+import { useUserStore } from "../../auth.ts"
+import Task from "../components/student-components/Task.vue"
 
 const userStore = useUserStore()
-
 const tasks = ref([])
 const loading = ref(true)
+const task = ref({
+  name: '',
+  due_date: '',
+  description: ''
+})
 
 onMounted(async () => {
   try {
-    const response = await fetch('http://localhost:8000/student-tasks/?username=' + userStore.user.username, {
+    const response = await fetch(`http://localhost:8000/student-tasks/?username=${userStore.user.username}`, {
       method: 'GET',
       credentials: "include"
     })
@@ -101,10 +104,8 @@ onMounted(async () => {
   }
 })
 
-const task = ref({
-  name: '',
-  due_date: '',
-  description: ''
+const sortedTasks = computed(() => {
+  return tasks.value.sort((a, b) => new Date(a.due_date) - new Date(b.due_date))
 })
 
 const updateTask = async (taskId, updatedTaskDetails) => {
@@ -238,10 +239,6 @@ h1 {
   position: relative;
   top: 0.25em;
   color: white;
-}
-
-.create {
-  font-weight: bold;
 }
 
 main {
